27c72b2c2fa6d53b6d75cba76e5993b15f2dcdb8
Prakash Kumar <prakashkumar100198@gmail.com>
2022-12-30 00:05:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 
 func initialMigration() *gorm.DB {
 
-	dbURI := "host=postgres-postgresql.env1 user=postgres dbname=postgres sslmode=disable password=prakash port=5432"
+	dbURI := "host=postgres-nats-test-postgresql.prakash user=postgres dbname=postgres sslmode=disable password=prakash port=5432"
 	//postgres-devtron-demo-postgresql-0
 	// url := "postgres://postgres:prakash@localhost:5432/postgres?sslmode=disable"
 	var dB *gorm.DB
